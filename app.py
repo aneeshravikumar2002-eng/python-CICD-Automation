0a533def9a0ea6f88e6c7c293d0fafe8bf6e1afc
Aneesh_Ravikumar <aneeshravikumar2002@gmail.com>
2026-03-04 11:52:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -140,9 +140,10 @@ def health():
 # Run Application
 # ----------------------------
 
+# ----------------------------
+# Run Application
+# ----------------------------
+
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=False
+    app.run(host="0.0.0.0", port=5000)
     )
